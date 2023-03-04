84ebc4d3068cf759884b9b0e446cbef1c12c76d5
panditdhamdhere <dhamdherepandit@gmail.com>
2023-03-04 23:29:10 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+
+function App() {
+    return 
+    <BrowserRouter>
+    <Routes>
+<Route path = "/" element = {} />
+<Route path = "/category/:id" = {} />
+<Route path = "/product" element = {} />
+    </Routes>
+    </BrowserRouter>
+}
+
+export default App;
