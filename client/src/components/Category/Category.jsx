cb0ad95bc42823c3ac33042d0f8335e299965455
panditdhamdhere <dhamdherepandit@gmail.com>
2023-03-05 00:10:50 +0530

    new added

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -1,6 +1,6 @@
 import "./Category.scss";
 const Category = () => {
-    return <div>Category</div>;
+    return <div>Category hai</div>;
 };
 
 export default Category;
---
+++ client/src/utils/context.js
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+const Context = createContext();
+
+const AppContext = ({ children }) => {
+    return (
+        <Context.Provider>
+            {children}
+        </Context.Provider>
+    )
+}
+
+export default AppContext;
